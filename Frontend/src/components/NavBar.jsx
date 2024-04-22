81f9ed95e0fac38116560eaa2c78a499b5e8fb3e
scheissman <zbaric_hr@yahoo.com>
2024-04-22 23:36:25 +0200

    Logout

--- Frontend/src/components/NavBar.jsx
+++ Frontend/src/components/NavBar.jsx
@@ -6,54 +6,66 @@ import logo from "../components/logo.png";
 import { RoutesNames } from "../constants";
 import { useNavigate } from "react-router-dom";
 import GornjiNav from "./gornjiNav";
+import { useContext } from 'react';
+import { AuthorizationContext } from '../components/AuthorizationContext';
+
 
 export default function NavBar() {
+  const { isLoggedIn, logout } = useContext(AuthorizationContext);
   const navigate = useNavigate();
 
   return (
-    <>
-      <GornjiNav></GornjiNav>
-      <Navbar expand="lg" className="bg-white">
-        <Container fluid className="p-0">
-          <Nav className="mx-right">
-            <Nav.Link onClick={() => navigate(RoutesNames.LOGIN)}>
-              Login
-            </Nav.Link>
-            <Nav.Link
-              target="blank"
-              href="https://scheissman-001-site1.ftempurl.com/swagger/index.html"
-            >
-              Api
-            </Nav.Link>
-            <NavDropdown
-              className="ms-auto"
-              title="za Crud op"
-              id="basic-nav-dropdown"
-            >
-              <NavDropdown.Item
-                onClick={() => navigate(RoutesNames.KORISNIK_PREGLED)}
-              >
-                Korisnici
-              </NavDropdown.Item>
-              <NavDropdown.Item
-                onClick={() => navigate(RoutesNames.UNOS_PREGLED)}
-              >
-                Unosi{" "}
-              </NavDropdown.Item>
-              <NavDropdown.Item
-                onClick={() => navigate(RoutesNames.RIBA_PREGLED)}
-              >
-                Ribe
-              </NavDropdown.Item>
-              <NavDropdown.Item
-                onClick={() => navigate(RoutesNames.ULOV_PREGLED)}
-              >
-                Ulovi
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Container>
-      </Navbar>
-    </>
+      <>
+          <GornjiNav></GornjiNav>
+          <Navbar expand="lg" className="bg-white">
+              <Container fluid className="p-0">
+                  <Nav className="mx-right">
+                      {isLoggedIn ? (
+                          <Nav.Link onClick={logout}>
+                              Logout
+                          </Nav.Link>
+                      ) : (
+                          <Nav.Link onClick={() => navigate(RoutesNames.LOGIN)}>
+                              Login
+                          </Nav.Link>
+                      )}
+
+                      <Nav.Link
+                          target="blank"
+                          href="https://scheissman-001-site1.ftempurl.com/swagger/index.html"
+                      >
+                          Api
+                      </Nav.Link>
+
+                      <NavDropdown
+                          className="ms-auto"
+                          title="za Crud op"
+                          id="basic-nav-dropdown"
+                      >
+                          <NavDropdown.Item
+                              onClick={() => navigate(RoutesNames.KORISNIK_PREGLED)}
+                          >
+                              Korisnici
+                          </NavDropdown.Item>
+                          <NavDropdown.Item
+                              onClick={() => navigate(RoutesNames.UNOS_PREGLED)}
+                          >
+                              Unosi
+                          </NavDropdown.Item>
+                          <NavDropdown.Item
+                              onClick={() => navigate(RoutesNames.RIBA_PREGLED)}
+                          >
+                              Ribe
+                          </NavDropdown.Item>
+                          <NavDropdown.Item
+                              onClick={() => navigate(RoutesNames.ULOV_PREGLED)}
+                          >
+                              Ulovi
+                          </NavDropdown.Item>
+                      </NavDropdown>
+                  </Nav>
+              </Container>
+          </Navbar>
+      </>
   );
 }
