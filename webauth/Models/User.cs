9f056457ea1b6b5718eb97e15fb44eb67958971f
scheissman <zbaric_hr@yahoo.com>
2024-05-03 13:34:42 +0200

    Gotovo

--- webauth/Models/User.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace webauth.Models
-{
-    public class User
-    {
-        public string Username { get; set; } = string.Empty;
-        public string PasswordHash { get; set; } = string.Empty;
-
-    }
-}
--- webauth/Models/UserDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace webauth.Models
-{
-    public class UserDto
-    {
-        public required string Username { get; set; }
-        public required string Password { get; set; }
-    }
-}
